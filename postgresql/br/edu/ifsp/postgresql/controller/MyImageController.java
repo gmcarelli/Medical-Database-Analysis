584288d8af260f60771a8379d95e611b6e9293db
Gil Carelli <gmcarelli@gmail.com>
2016-04-29 11:05:43 -0300

    updating last version

--- postgresql/br/edu/ifsp/postgresql/controller/MyImageController.java
+++ postgresql/br/edu/ifsp/postgresql/controller/MyImageController.java
@@ -4,12 +4,12 @@ import br.edu.ifsp.model.MyImage;
 import br.edu.ifsp.postgresql.view.JFrameMainView;
 
 public class MyImageController {
-		
+
 	private MyImage myImage;
 	private JFrameMainView mainView;
-	
+
 	public MyImageController(MyImage myImage, JFrameMainView mainView) {
-		
+
 	}
 
 	public MyImage getMyImage() {
@@ -28,6 +28,25 @@ public class MyImageController {
 		this.mainView = mainView;
 	}
 
-	
-	
+	public boolean isImageValid() {
+
+		if (this.myImage != null) {
+
+			if (this.myImage.getImageName() == null || this.myImage.getImageName().equals("")) {
+				return false;
+			}
+
+			if (this.myImage.getImageBytes() == null || this.myImage.getImageBytes().length == 0) {
+				return false;
+			}
+
+			return true;
+			
+		} else {
+
+			return false;
+			
+		}
+	}
+
 }
