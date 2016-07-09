6ddb17efacc75e9205ec88624a6a42df15763560
Lucas Venezian Povoa <lucasvenez@gmail.com>
2016-07-09 12:19:04 -0300

    Update build and performance analysis scripts

--- performance-analysis.sh
+++ performance-analysis.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
-# It is a script at aming to deploy the medical-database-analysis application 
-# and setup the envoriment to run experiment to analysis the persistence and 
-# retrievement performance of relational and no relational database systems
+# It is a script at aiming to deploy the medical-database-analysis application 
+# and setup the environment to run experiment to analysis the persistence and 
+# retrieval performance of relational and non relational database systems
 # into a Unix-like operating system. How to use:
 #
-# sudo deploy.sh <database-system> <username> <password> <number-of-tests>
+# sudo deploy.sh <database-system> <username> <password> <number-of-tests> <operation>
 # <database-system> is the database system, which may be defined as: pgsql, mongo, or neo4j
 # <username> is the username of the database system
 # <password> is the user password of the database system
-# <number-of-test> is the number of test to be executed sequentily 
+# <number-of-test> is the number of test to be executed sequentially 
 #
 # Getting parameters
 #
@@ -18,14 +18,15 @@ username=$2
 password=$3
 numberOfTests=$4
 operation=$5
-imagesDirectory=$6
-
+imagesDirectoryOrImagesId=$6
 #
 # Setting database admin command
 #
 databaseadmin=""
-
-case "$1" in
+#
+#
+#
+case "$database" in
 pgsql)
    databaseadmin="/etc/init.d/postgresql"
    ;;
@@ -36,47 +37,76 @@ neo4j)
    databaseadmin="/etc/init.d/neo4j"
    ;;
 *)
-   echo "Usage deploy.sh <database-system> <username> <password> <number-of-tests>"
+   echo "Usage performance-test.sh <database-system> <username> <password> <number-of-tests> <operation> <imagepathorids>"
+   exit 1
+esac
+#
+# Checking operation
+#
+case "$operation" in
+-p)
+   ;;
+-r)
+   ;;
+*)
+   echo "Operation $operation not supported"
    exit 1
 esac
 #
 #
 #
+numberOfImages=0
 i=1
 images=""
-if [[ -d $imagesDirectory ]]; then
-   for file in $imagesDirectory/*; do
-      if [[ -f $file ]]; then
-         if 
-         images="$images $i $(basename $file) $file"
-         i=$((i+1))
-      fi
-   done	
+if [ $operation = "-p" ]; then
+	if [[ -d $imagesDirectoryOrImagesId ]]; then
+    	for file in $imagesDirectoryOrImagesId/*; do
+        	if [[ -f $file ]]; then
+            	images="$images $i $(basename $file) $file"
+    			i=$((i+1))
+    			numberOfImages=$((numberOfImages+1))
+        	fi
+    	done	
+	fi
 fi
-
-echo $images
-exit 0
+#
+#
+#
+if [ $operation = "-r" ]; then
+	images=$imagesDirectoryOrImagesId;
+	IFS=' ' read -r -a array <<< "$images"
+	numberOfImages=${#array[@]}
+fi
 #
 # Creating file name
 #
-file="$database-times.csv"
+file="$database$operation-$numberOfTests.csv"
 #
 # Executing tests 
 #
 for i in `seq 1 $numberOfTests`; do
-   # 
-   # Clearning memory and swap before execute tests
-   # Script indicated at http://www.tecmint.com/clear-ram-memory-cache-buffer-and-swap-space-on-linux/
-   #
-   echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'
-   #
-   # Restating database server
-   #
-   $databaseadmin stop
-   $databaseadmin start
-   #
-   # Running application 
-   #
-   t=`time java -jar mda.jar $database MEDICALIMAGE $username $password $operation $images'
-   echo "$database;$numberOfTests;$operation;$numberOfImages;$t"
+	#
+	#
+	#
+	echo "Executing test $i of $numberOfTests"
+   	# 
+   	# Clearing memory and swap before execute tests
+   	# Script indicated at http://www.tecmint.com/clear-ram-memory-cache-buffer-and-swap-space-on-linux/
+   	#
+	echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a
+   	#
+   	# Restating database server
+   	#
+	#$databaseadmin stop
+	#$databaseadmin start
+   	#
+   	# Running application 
+   	#
+	t=`time java -jar mda.jar $database MEDICALIMAGE $username $password $operation $images`
+	if [ "$operation" = "-p" ]; then
+		ope="persistence"
+	else
+		ope="retrieve"
+	fi
+	echo "$database;$numberOfTests;$ope;$numberOfImages;$t"
 done
