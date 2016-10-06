8d48683b1179adb1c3be3168f2e2a3f53695c7bf
Lucas Venezian Povoa <lucasvenez@gmail.com>
2016-10-05 23:10:54 -0300

    Add analysis data

---
+++ R/histogram.r
@@ -0,0 +1,19 @@
+require("ggplot2")
+
+d <- read.table("data/psql-mongo-persistence-analysis-02-16.tsv", sep = "\t", dec = ",")
+
+colnames(d) <- c("database", "iterations", "operation", "images", "usertime", "cputime", "totaltime")
+
+d <- d[-301,]
+
+d$operation <- as.character(d$operation)
+
+d$operation <- replace(d$operation, d$operation == "persistence", "Inclusão")
+d$operation <- replace(d$operation, d$operation == "retrieve", "Recuperação")
+
+d$operation <- as.factor(d$operation)
+
+gplot <- ggplot(d) + geom_histogram(aes(totaltime)) + facet_grid(database ~ operation) + 
+  xlab("Millisegundos") + ylab("Frequência")
+
+ggsave(plot = gplot, filename = "images/histogram.png", units = "cm", height = 8, width = 10)
